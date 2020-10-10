b79f16adb77529da1ef98d1880e39712a72248e8
wangxiaorui <wangxiaorui@pminfo.cn>
2020-10-10 16:39:32 +0800

    1.0版完成

--- web/src/main/java/com/webstudy/web/controller/WebController.java
+++ web/src/main/java/com/webstudy/web/controller/WebController.java
@@ -2,6 +2,8 @@ package com.webstudy.web.controller;
 
 import com.serverstudy.server.openapi.OpenApi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,11 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/testWeb")
 public class WebController {
 
+    @Value("${aa.bb}")
+    private String value;
+
     @Autowired
     private OpenApi openApi;
 
     @RequestMapping("/getWeb")
     public String get() {
         return openApi.get("通过web调用");
+    }
+
+    @RequestMapping("/getValue")
+    public String value() {
+        return "value"+value;
     }
 }
